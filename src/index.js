089102d77d9a9be85b9a4d2e9942dc3b328d34c3
dcrimsoft <103871334+dcrimsoft@users.noreply.github.com>
2022-04-22 03:58:19 +0300

    add logo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ function MainContent() {
         <div className="main_page">
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container">
-                    <a className="navbar-brand" href="#"><span className="lg_span">i-</span>Minter</a>
+                    <a className="navbar-brand" href="#">
+                        <img src="./src/images/home_logo.PNG"></img>
+                    </a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
